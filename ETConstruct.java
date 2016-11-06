b3c19af24e3fe52f57abeb3f0543fc6d1f02f6ab
zhangyuwinnie <zhangyuwinnie@tamu.edu>
2016-11-06 12:21:15 -0600

    modify ETConstruct to deal with case when no where clause, but have some bugs

--- ETConstruct.java
+++ ETConstruct.java
@@ -27,23 +27,44 @@ public class ETConstruct {
 			tables_for_cross.add(new ExpressionTree(parsetree.getChildren().get(1).getChildren().get(i).getSymbol()));
 		}
 		ExpressionTree cross = new ExpressionTree("cross",tables_for_cross,null);
+
 		
-		//node of "sigma"
-		List<ExpressionTree> list = new ArrayList<ExpressionTree>();
-	    list.add(cross);
-		ExpressionTree sigma = new ExpressionTree("sigma",list,parsetree.getChildren().get(2).getChildren());
-		
-		// node of "pi"
-		List<ParseTree> names_for_select = new ArrayList<ParseTree>();
-		// get the number of children for "table_list"
-		int num = parsetree.getChildren().get(0).getChildren().size();
-		//System.out.println(parsetree.getChildren().get(1).getChildren().get(0).getSymbol());
-		for(int i = 0; i < num; i++){	
-			names_for_select.add(parsetree.getChildren().get(0).getChildren().get(i));
+		int n = parsetree.getChildren().size();
+		if (n == 3){
+			//node of "sigma"
+			List<ExpressionTree> list = new ArrayList<ExpressionTree>();
+		    list.add(cross);
+			ExpressionTree sigma = new ExpressionTree("sigma",list,parsetree.getChildren().get(2).getChildren());
+			
+			// node of "pi"
+			List<ParseTree> names_for_select = new ArrayList<ParseTree>();
+			// get the number of children for "table_list"
+			int num = parsetree.getChildren().get(0).getChildren().size();
+			//System.out.println(parsetree.getChildren().get(1).getChildren().get(0).getSymbol());
+			for(int i = 0; i < num; i++){	
+				names_for_select.add(parsetree.getChildren().get(0).getChildren().get(i));
+			}
+			List<ExpressionTree> list1 = new ArrayList<ExpressionTree>();
+		    list1.add(sigma);
+			ExpressionTree pi = new ExpressionTree("pi",list1,names_for_select);
+			System.out.println("*****");
 		}
-		List<ExpressionTree> list1 = new ArrayList<ExpressionTree>();
-	    list1.add(sigma);
-		ExpressionTree pi = new ExpressionTree("pi",list1,names_for_select);
+		else{
+			// node of "pi"
+			List<ParseTree> names_for_select = new ArrayList<ParseTree>();
+			// get the number of children for "table_list"
+			int num = parsetree.getChildren().get(0).getChildren().size();
+			//System.out.println(parsetree.getChildren().get(1).getChildren().get(0).getSymbol());
+			for(int i = 0; i < num; i++){	
+				names_for_select.add(parsetree.getChildren().get(0).getChildren().get(i));
+			}
+			List<ExpressionTree> list1 = new ArrayList<ExpressionTree>();
+		    list1.add(cross);
+			ExpressionTree pi = new ExpressionTree("pi",list1,names_for_select);
+			System.out.println("&&&&&");
+		}
+		
+		
 		return dull;
 	}
 	
--- Lexer.java
+++ Lexer.java
@@ -83,7 +83,7 @@ public class Lexer {
 
 		public static void main(String[] args) {
 		    //String input = "SELECT * FROM course WHERE grade = \"C\" AND [ exam > 70 OR project > 70 ] AND NOT ( exam * 30 + homework * 20 + project * 50 ) / 100 < 60";
-			String select_statement = "SELECT (zy, ww) FROM course, course2 WHERE course.sid = course2.sid AND course.exam > course2.exam;";
+			String select_statement = "SELECT (zy, ww) FROM course, course2 ;";
 		    // Create tokens and print them
 		    //ArrayList<Token> tokens = lex(input);
 			ArrayList<Token> tokens = lex(select_statement);
@@ -95,7 +95,6 @@ public class Lexer {
 		    PTConstruct con = new PTConstruct(mylist);
 		    ParseTree t;
 		    t = con.construct();
-		    System.out.println("stop1");
 		    ETConstruct et = new ETConstruct(t);
 		    ExpressionTree e;
 		    e = et.construct();
